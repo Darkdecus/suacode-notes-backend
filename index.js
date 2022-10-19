7d56febc594c94a915373c8199240f3ccfa4b12b
Darkdecus <pkaabudu@gmail.com>
2022-10-19 03:48:29 +0000

    change  urlendcoded option

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = require("./router");
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Import Routes
