ac33f9ebfbb042e6ac1f485629b97e78fd073b0c
Darkdecus <pkaabudu@gmail.com>
2022-10-20 06:51:28 +0000

    error handling extended for create notes route

--- Notes/Note.controller.js
+++ Notes/Note.controller.js
@@ -1,7 +1,7 @@
 const Note = require("./Note.schema");
 
 exports.findAll = async (req, res) => {
-  return res.json(await Note.find());
+  return res.json(await Note.find().sort("-updatedAt"));
 };
 
 exports.create = async (req, res) => {
@@ -9,11 +9,15 @@ exports.create = async (req, res) => {
     const { body } = req;
     if (!body.title && !body.body)
       return res.status(400).json({ msg: "missing parameters in body" });
-    else if (await Note.find({ title: body.title }))
+    else if (await Note.findOne({ title: body.title }))
       return res.status(400).json({ msg: "title must be unique" });
     else return res.status(201).json(await Note.create(body));
   } catch (error) {
-    console.log(error);
+    console.log(JSON.stringify(error, null, 3));
+    if (error?.errors?.body.name === "ValidatorError")
+      return res
+        .status(400)
+        .json({ msg: error?.errors?.body.message.split("Path ")[1] });
     return res.status(500).json({ msg: "something went wrong" });
   }
 };
@@ -27,10 +31,10 @@ exports.checkTitle = async (req, res) => {
 exports.update = async (req, res) => {
   return res
     .status(201)
-    .json(await Note.findByIdAndUpdate(req.param.id, req.body));
+    .json(await Note.findByIdAndUpdate(req.params.id, req.body));
 };
 
 exports.delete = async (req, res) => {
-  await Note.findByIdAndDelete(req.param.id);
+  await Note.findByIdAndDelete(req.params.id);
   return res.status(204).json();
 };
